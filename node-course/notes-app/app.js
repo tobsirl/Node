9a4b9d72d2abc2ccb71b4192d0249905ecd68d77
Paul Tobin <tobster111@gmail.com>
2020-06-09 20:56:50 +0100

    Setting up commands for add, remove, list and read

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,8 +1,41 @@
 const validator = require('validator');
 const chalk = require('chalk');
-const argv = require('yargs').argv;
+const yargv = require('yargs');
 
+// Create the add command
+yargv.command({
+  command: 'add',
+  describe: 'Adds a new note',
+  handler: function () {
+    console.log(`Adding a new note`);
+  },
+});
 
-console.log(process.argv);
-console.log(argv._[0]);
+// Create the remove command
+yargv.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log(`Remove a note`);
+  },
+});
 
+// List the notes
+yargv.command({
+  command: 'list',
+  describe: 'List all the notes',
+  handler: function () {
+    console.log(`List all the notes`);
+  },
+});
+
+// Read a note
+yargv.command({
+  command: 'read',
+  describe: 'Read a specific note',
+  handler: function () {
+    console.log(`Read a specific note`);
+  },
+});
+
+console.log(yargv.argv);
