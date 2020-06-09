b2c5f44078df7ec1be79dc12b8cf8a836a0cfa90
Paul Tobin <tobster111@gmail.com>
2020-06-09 16:28:38 +0100

    Basic Node - Used validator to check if an email is valid using the isEmail method

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,5 +1,10 @@
+const validator = require('validator');
+
 const { getNotes } = require('./notes');
 
 const notes = getNotes();
 
+const isEmail = validator.isEmail('paul@google.com');
+
 console.log(notes);
+console.log(isEmail);
