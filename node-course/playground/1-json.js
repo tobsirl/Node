dd7ccf1abbc834ae067d90d578cea68cde8b799d
Paul Tobin <tobster111@gmail.com>
2020-06-10 19:01:28 +0100

    Completed the challenge to read and write JSON data to the file

--- node-course/playground/1-json.js
+++ node-course/playground/1-json.js
@@ -12,8 +12,20 @@ const fs = require('fs');
 
 // fs.writeFileSync('1-json.json', bookJson);
 
+// const dataBuffer = fs.readFileSync('1-json.json');
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+
+// console.log(data);
+
 const dataBuffer = fs.readFileSync('1-json.json');
 const dataJSON = dataBuffer.toString();
 const data = JSON.parse(dataJSON);
 
+data.name = 'Paul';
+data.age = 40;
+
 console.log(data);
+
+const userJSON = JSON.stringify(data);
+fs.writeFileSync('1-json.json', userJSON);
