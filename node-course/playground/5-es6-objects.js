04c5d4f559802be85a703f8cf1590c0dcc935d19
Paul Tobin <tobster111@gmail.com>
2020-06-15 16:04:08 +0100

    Object short hand syntax and destructuring

---
+++ node-course/playground/5-es6-objects.js
@@ -0,0 +1,24 @@
+// Object property shorthand
+
+const name = 'Andrew';
+const userAge = 27;
+
+const user = {
+  name,
+  age: userAge,
+  location: 'Philadelphia',
+};
+
+console.log(user);
+
+// Object destructuring
+const product = {
+  label: 'Red notebook',
+  price: 3,
+  stock: 201,
+  salePrice: undefined,
+};
+
+// const label = product.label
+
+const { label, price, stock, salePrice } = product;
