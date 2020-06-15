de9c07cf6746ecd19666c39a3ce5ec9ec39fe014
Paul Tobin <tobster111@gmail.com>
2020-06-15 15:26:59 +0100

    Callback chaining

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -4,9 +4,15 @@ require('dotenv').config();
 const { getGeocode } = require('./utils/getGeocode');
 const { forecast } = require('./utils/forecast');
 
-forecast();
 
-getGeocode('Clonmel').catch((err) =>
-  console.log(`Error Message: ${err.message}`)
-);
-getGeocode('New York').catch((err) => console.log(err.message));
+
+// getGeocode('Clonmel').catch((err) =>
+//   console.log(`Error Message: ${err.message}`)
+// );
+// getGeocode('New York').catch((err) => console.log(err.message));
+
+console.log(process.argv[2])
+
+getGeocode(process.argv[2])
+
+forecast();
--- node-course/weather-app/utils/forecast.js
+++ node-course/weather-app/utils/forecast.js
@@ -1,10 +1,10 @@
 const fetch = require('node-fetch');
 
-exports.forecast = async () => {
+exports.forecast = async (lat, long) => {
   const { ACCESS_KEY } = process.env;
   try {
     const response = await fetch(
-      `http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=Clonmel`
+      `http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${lat},${long}`
     );
     const data = await response.json();
     displayCurrentWeather(data);
@@ -14,9 +14,10 @@ exports.forecast = async () => {
 };
 
 const displayCurrentWeather = (weather) => {
-  const { temperature, wind_speed } = weather.current;
+  const { temperature, wind_speed, observation_time } = weather.current;
   const { name, country, region } = weather.location;
   console.log(`Location: ${name}, ${region}, ${country}`);
   console.log(`Temp: ${temperature}`);
-  console.log(`Temp: ${wind_speed}`);
+  console.log(`Wind Speed: ${wind_speed}`);
+  console.log(observation_time);
 };
