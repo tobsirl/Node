734f3000fabf8bcb9554b25a1031bc8dd17554ac
Paul Tobin <tobster111@gmail.com>
2020-06-16 12:52:32 +0100

    Setup a few routes

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -2,5 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.status(200).send(`Welcome from the server`);
+});
 
+app.get('/help', (req, res) => {
+  res.status(200).send('help page');
+});
 
+app.get('/about', (req, res) => {
+  res.status(200).send(`About page`)
+})
+
+app.get('/weather', (req, res) => {
+  res.status(200).json({
+    location: 'Ireland',
+    rain: 'Lots of rain',
+  });
+});
+
+app.listen(3000, () => console.log(`Server running on 3000`));
