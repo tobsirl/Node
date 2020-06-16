01aefaa84b6dcca3e917f542002ff1e016dbf735
Paul Tobin <tobster111@gmail.com>
2020-06-16 17:10:51 +0100

    Setting up partials with hbs

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -1,15 +1,17 @@
 const path = require('path');
 const express = require('express');
-const hbs = require('hbs')
+const hbs = require('hbs');
 
 const app = express();
 
 // Define paths for Express config
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(path.join(__dirname, '../public/')));
