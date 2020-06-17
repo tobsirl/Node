f00ba32c5e7eb67439a3ecb716b82c8a88d61ffe
Paul Tobin <tobster111@gmail.com>
2020-06-17 17:22:19 +0100

    Further styling

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, '../public/')));
 
 app.get('', (req, res) => {
   res.render('index', {
-    title: 'Weather App',
+    title: 'Weather',
     name: 'Paul Tobin',
   });
 });
