bb3e6352d77f65bd5cc2c7229fd6d2a46a2eb93b
Paul Tobin <tobster111@gmail.com>
2020-06-17 14:51:22 +0100

    Added 404.hbs with two routes

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -45,4 +45,20 @@ app.get('/weather', (req, res) => {
   });
 });
 
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    errorMessage: 'Help article not found',
+    name: 'Paul Tobin',
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    errorMessage: 'Page not found',
+    name: 'Paul Tobin',
+  });
+});
+
 app.listen(3000, () => console.log(`Server running on 3000`));
