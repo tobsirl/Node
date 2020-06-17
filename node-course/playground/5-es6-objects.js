e8a9f19baba0a7c1df264f3f109a7e46e78291c5
Paul Tobin <tobster111@gmail.com>
2020-06-17 14:34:35 +0100

    Added a footer.hbs

--- node-course/playground/5-es6-objects.js
+++ node-course/playground/5-es6-objects.js
@@ -22,3 +22,21 @@ const product = {
 // const label = product.label
 
 const { label, price, stock, salePrice } = product;
+
+// Computed Property Names old way
+// function objectify(key, value) {
+//   let obj = {};
+//   obj[key] = value;
+//   return obj;
+// }
+
+// objectify('name', 'Paul'); //?
+
+function objectify(key, value) {
+  return {
+    [key]: value,
+  };
+}
+
+objectify('name', 'Paul'); //?
+
