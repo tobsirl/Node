78778a7a2da9927598c73638b5bff33d9672c32d
Paul Tobin <tobster111@gmail.com>
2020-06-19 16:45:30 +0100

    Setup new application, make connection to the mongodb database

---
+++ node-course/task-manager/mongodb.js
@@ -0,0 +1,7 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient();
+
+const connectionURL = `mongodb://127.0.0.1:27017`;
+const databaseName = `tast-manager`;
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true });
