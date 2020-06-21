85be79406ad2742e1319ce7106d2c4fb791cfe50
Paul Tobin <tobster111@gmail.com>
2020-06-21 09:13:45 +0100

    Creating a new User and saving to the database

--- node-course/task-manager/src/db/mongoose.js
+++ node-course/task-manager/src/db/mongoose.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
 const User = mongoose.model('User', {
@@ -13,3 +14,13 @@ const User = mongoose.model('User', {
     type: Number,
   },
 });
+
+const me = new User({ name: 'Kennet', age: 23 });
+
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
