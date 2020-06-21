44913caa0a0bd87ae28a54f98bdaf8855aebdc86
Paul Tobin <tobster111@gmail.com>
2020-06-21 10:59:56 +0100

    Using properties such as default, trim and lowercase

--- node-course/task-manager/src/db/mongoose.js
+++ node-course/task-manager/src/db/mongoose.js
@@ -14,10 +14,13 @@ const User = mongoose.model('User', {
   name: {
     type: String,
     required: true,
+    trim: true,
   },
   email: {
     type: String,
     required: true,
+    trim: true,
+    lowercase: true,
     validate(value) {
       if (!validator.isEmail(value)) {
         throw new Error('Email is invalid');
@@ -26,6 +29,7 @@ const User = mongoose.model('User', {
   },
   age: {
     type: Number,
+    default: 0,
     validate(value) {
       if (value < 0) throw new Error(`Age must be a positive number`);
     },
