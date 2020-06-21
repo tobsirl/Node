19fad2a8f146cd7bad0a04c9ffeefb04f726e27c
Paul Tobin <tobster111@gmail.com>
2020-06-21 09:59:27 +0100

    Creating a new Task and saving it to the db

--- node-course/task-manager/src/db/mongoose.js
+++ node-course/task-manager/src/db/mongoose.js
@@ -1,26 +1,49 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
-
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-});
-
-const me = new User({ name: 'Kennet', age: 23 });
-
-me.save()
-  .then(() => {
-    console.log(me);
+mongoose
+  .connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
   })
-  .catch((error) => {
-    console.log(error);
+  .then(() => console.log(`Connected to the database`))
+  .catch((err) => console.log(err));
+
+// const User = mongoose.model('User', {
+//   name: {
+//     type: String,
+//   },
+//   age: {
+//     type: Number,
+//   },
+// });
+
+// const me = new User({ name: 'Kennet', age: 23 });
+
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+const Task = mongoose.model('Task', {
+  description: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const task = new Task({ description: 'Task 1', completed: true });
+
+task
+  .save()
+  .then(() => console.log(task))
+  .catch((err) => {
+    console.log(err);
   });
