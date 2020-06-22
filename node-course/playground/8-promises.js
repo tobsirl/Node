cf9a7ddc28416cf8979727b2aff313e5e893aa9b
Paul Tobin <tobster111@gmail.com>
2020-06-22 17:36:42 +0100

    Example of chaining by calling the add function again, problem nesting for each call

--- node-course/playground/8-promises.js
+++ node-course/playground/8-promises.js
@@ -1,21 +1,45 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // resolve([7, 4, 1]);
-    reject(`Things went wrong!`);
-  }, 2000);
-});
+// const doWorkPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+// resolve([7, 4, 1]);
+//     reject(`Things went wrong!`);
+//   }, 2000);
+// });
 
-doWorkPromise
-  .then((result) => {
-    console.log('Success', result);
-  })
-  .catch((error) => {
-    console.log(`Error:`, error);
+// doWorkPromise
+//   .then((result) => {
+//     console.log('Success', result);
+//   })
+//   .catch((error) => {
+//     console.log(`Error:`, error);
+//   });
+
+//                                fulfilled
+//                              /
+// Promise      -- pending -->
+//                              \
+//                                rejected
+
+// nested for each call made
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b);
+    }, 2000);
   });
+};
 
+add(1, 2)
+  .then((sum) => {
+    console.log(sum);
 
-  //                                fulfilled
-  //                              /
-  // Promise      -- pending -->
-  //                              \
-  //                                rejected
+    add(sum, 5)
+      .then((sum2) => {
+        console.log(sum2);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
