c4231333f5605f3bce24c38ed7ce7565920de88f
Paul Tobin <tobster111@gmail.com>
2020-06-24 15:06:07 +0100

    Refactored - moved functions out of routes in controllers

--- node-course/task-manager/src/controllers/userController.js
+++ node-course/task-manager/src/controllers/userController.js
@@ -18,3 +18,122 @@ exports.getUsers = async (req, res) => {
     });
   }
 };
+
+exports.getUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).send(`User not found!`);
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'failed',
+      error,
+    });
+  }
+};
+
+exports.createUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const newUser = new User({
+    name,
+    email,
+    password,
+  });
+
+  try {
+    await newUser.save();
+    res.status(201).json({
+      status: 'success',
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'failed',
+      error,
+    });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { name, email, password } = req.body;
+
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).json({
+      status: 'failed',
+      message: 'Invalid Updates',
+    });
+  }
+
+  try {
+    const updateUser = await User.findByIdAndUpdate(
+      id,
+      {
+        name,
+        email,
+        password,
+      },
+      { new: true, runValidators: true }
+    );
+
+    if (!updateUser) {
+      return res.status(404).json({
+        status: 'failed',
+        message: 'User not found',
+      });
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        updateUser,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'failed',
+      error,
+    });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const deleteUser = await User.findByIdAndDelete(req.params.id);
+
+    if (!deleteUser) {
+      return res.status(404).json({
+        status: 'failed',
+        message: 'User not found',
+      });
+    }
+
+    res.status(200).json({
+      status: 'success',
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'failed',
+    });
+  }
+};
--- node-course/task-manager/src/routes/userRoutes.js
+++ node-course/task-manager/src/routes/userRoutes.js
@@ -1,133 +1,25 @@
 const express = require('express');
 
-// Models
-const User = require('../models/user');
-
 // Controllers
-const { getUsers } = require('../controllers/userController');
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../controllers/userController');
 
 const router = express.Router();
 
 // user endpoints
 router.get('/', getUsers);
 
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
+router.get('/:id', getUser);
 
-  try {
-    const user = await User.findById(id);
+router.post('/', createUser);
 
-    if (!user) {
-      return res.status(404).send(`User not found!`);
-    }
+router.patch('/:id', updateUser);
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        user,
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: 'failed',
-      error,
-    });
-  }
-});
-
-router.post('/', async (req, res) => {
-  const { name, email, password } = req.body;
-
-  const newUser = new User({
-    name,
-    email,
-    password,
-  });
-
-  try {
-    await newUser.save();
-    res.status(201).json({
-      status: 'success',
-      data: {
-        user: newUser,
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: 'failed',
-      error,
-    });
-  }
-});
-
-router.patch('/users/:id', async (req, res) => {
-  const { id } = req.params;
-  const { name, email, password } = req.body;
-
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ['name', 'email', 'password', 'age'];
-  const isValidOperation = updates.every((update) =>
-    allowedUpdates.includes(update)
-  );
-
-  if (!isValidOperation) {
-    return res.status(400).json({
-      status: 'failed',
-      message: 'Invalid Updates',
-    });
-  }
-
-  try {
-    const updateUser = await User.findByIdAndUpdate(
-      id,
-      {
-        name,
-        email,
-        password,
-      },
-      { new: true, runValidators: true }
-    );
-
-    if (!updateUser) {
-      return res.status(404).json({
-        status: 'failed',
-        message: 'User not found',
-      });
-    }
-
-    res.status(200).json({
-      status: 'success',
-      data: {
-        updateUser,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: 'failed',
-      error,
-    });
-  }
-});
-
-router.delete('/users/:id', async (req, res) => {
-  try {
-    const deleteUser = await User.findByIdAndDelete(req.params.id);
-
-    if (!deleteUser) {
-      return res.status(404).json({
-        status: 'failed',
-        message: 'User not found',
-      });
-    }
-
-    res.status(200).json({
-      status: 'success',
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: 'failed',
-    });
-  }
-});
+router.delete('/:id', deleteUser);
 
 module.exports = router;
