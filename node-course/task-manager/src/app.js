a71b8e40d392b81d4a9115d527a6e05f4485f7c4
Paul Tobin <tobster111@gmail.com>
2020-06-25 15:46:05 +0100

    Using bcryptjs to hash and compare passwords

--- node-course/task-manager/src/app.js
+++ node-course/task-manager/src/app.js
@@ -1,15 +1,32 @@
-const express = require('express');
+// const express = require('express');
 
-// controllers
-const userRouter = require('./routes/userRoutes');
-const taskRouter = require('./routes/taskRoutes');
+// // controllers
+// const userRouter = require('./routes/userRoutes');
+// const taskRouter = require('./routes/taskRoutes');
 
-const app = express();
+// const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
-// Routes
-app.use('/users/', userRouter);
-app.use('/tasks/', taskRouter);
+// // Routes
+// app.use('/users/', userRouter);
+// app.use('/tasks/', taskRouter);
 
-module.exports = app;
+const bcrypt = require('bcryptjs');
+
+const myFunction = async () => {
+  const password = 'JuAW2Zz3sfdvNPWc6Whm';
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  console.log(`Password: ${password}`);
+  console.log(`Hashed: ${hashed}`);
+
+  const isMatch = await bcrypt.compare(password, hashed);
+
+  console.log(isMatch);
+};
+
+myFunction();
+
+// module.exports = app;
