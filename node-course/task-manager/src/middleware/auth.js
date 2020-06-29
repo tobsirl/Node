9ecedc1d546f167812c547e532443000a41f4d79
Paul Tobin <tobster111@gmail.com>
2020-06-29 16:23:45 +0100

    Auth middleware using jwt.verify()

--- node-course/task-manager/src/middleware/auth.js
+++ node-course/task-manager/src/middleware/auth.js
@@ -3,15 +3,26 @@ const jwt = require('jsonwebtoken');
 
 exports.auth = async (req, res, next) => {
   try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+
+    console.log(token);
+    const decoded = jwt.verify(token, 'thecakeisalie');
+
+    console.log(decoded);
+
+    const user = await User.findOne({
+      _id: decoded._id,
+      'tokens.token': token,
+    });
+
+    if (!user) {
+      throw new Error();
+    }
+
+    req.user = user;
+
+    next();
   } catch (error) {
     res.status(401).send({ error: 'Please authenticate' });
   }
-
-  const bearerToken = req.headers.authorization;
-
-  const token = bearerToken.split(' ')[1];
-
-  console.log(token);
-
-  next();
 };
