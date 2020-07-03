b3addd0bc4dc617b78ccd736d1e80cd033ae3415
Paul Tobin <tobster111@gmail.com>
2020-07-03 16:17:02 +0100

    Restricting filesize to 1MB and filetype jpg, jpeg and png

--- node-course/task-manager/src/routes/userRoutes.js
+++ node-course/task-manager/src/routes/userRoutes.js
@@ -6,6 +6,13 @@ const upload = multer({
   limits: {
     fileSize: 1000000,
   },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error(`Please upload a jpg, jpeg, png`));
+    }
+
+    cb(undefined, true);
+  },
 });
 
 // Controllers
