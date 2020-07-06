23d82e91c7317709d1a074797c88a6bdfb032cda
Paul Tobin <tobster111@gmail.com>
2020-07-06 14:55:37 +0100

    Added a public folder with index.html

--- node-course/chat-app/src/index.js
+++ node-course/chat-app/src/index.js
@@ -1,8 +1,9 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
-
+app.use(express.static(path.join(__dirname, '../public')));
 
 const PORT = process.env.PORT || 3000;
 
