6e90b643be9e83c94efaad3d4aeed1dd38f76210
Paul Tobin <tobster111@gmail.com>
2020-07-06 15:14:11 +0100

    Setup start and dev script

--- node-course/chat-app/src/index.js
+++ node-course/chat-app/src/index.js
@@ -7,4 +7,4 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
