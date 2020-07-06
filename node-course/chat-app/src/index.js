e312db4dd550bfea57e69340ce2a9671fa934480
Paul Tobin <tobster111@gmail.com>
2020-07-06 15:35:36 +0100

    Installed socket.io, setup the server using http and made the server side connection

--- node-course/chat-app/src/index.js
+++ node-course/chat-app/src/index.js
@@ -1,10 +1,18 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, '../public')));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+io.on('connection', () => {
+  console.log(`New WebSocket connection`);
+});
+
+server.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
