36a2325dc02c604e2a5caba9e9006741368c4024
Paul Tobin <tobster111@gmail.com>
2020-07-07 15:18:45 +0100

    Challenge complete

--- node-course/chat-app/public/js/chat.js
+++ node-course/chat-app/public/js/chat.js
@@ -1,14 +1,18 @@
 const socket = io();
 
-socket.on('countUpdated', (count) => {
-  console.log(`The count has been updated!`, count);
+socket.on('message', (message) => {
+  console.log(message);
 });
 
-document.getElementById('increment').addEventListener('click', () => {
-  // console.log(`Clicked`);
-  socket.emit('increment');
-});
+// socket.on('countUpdated', (count) => {
+//   console.log(`The count has been updated!`, count);
+// });
 
-document.getElementById('toggleColor').addEventListener('click', () => {
-  socket.emit('toggleColor')
-})
+// document.getElementById('increment').addEventListener('click', () => {
+//   // console.log(`Clicked`);
+//   socket.emit('increment');
+// });
+
+// document.getElementById('toggleColor').addEventListener('click', () => {
+//   socket.emit('toggleColor')
+// })
