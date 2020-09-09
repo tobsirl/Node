41d25fc34e4984c5567b3cc55306066f8012d72f
Paul Tobin <tobster111@gmail.com>
2020-09-09 16:45:31 +0100

    Added the toMeta and output functions

--- io/fetching-meta-data/meta.js
+++ io/fetching-meta-data/meta.js
@@ -18,3 +18,34 @@ function print(dir) {
     .forEach(output);
   write.newline();
 }
+
+function toMeta({ file, dir }) {
+  const stats = fs.statSync(path.join(dir, file));
+  var { birthtime, ino, mode, nlink, size } = stats;
+  birthtime = birthtime.toUTCString();
+  mode = mode.toString(8);
+  size += 'B';
+  return {
+    file,
+    dir,
+    info: [birthtime, ino, mode, nlink, size],
+    isDir: stats.isDirectory(),
+  };
+}
+
+function output({ file, dir, info, isDir }) {
+  write(file, ...info);
+  if (!isDir) {
+    return;
+  }
+  const p = path.join(dir, file);
+  write.arrow();
+  fs.readdirSync(p).forEach((f) => {
+    const stats = fs.statSync(path.join(p, f));
+    const style = stats.isDirectory() ? 'bold' : 'dim';
+    write[style](f);
+  });
+  write.newline();
+}
+
+print(process.argv[2] || '.');
