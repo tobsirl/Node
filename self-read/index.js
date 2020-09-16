e4897533eccc1b2b9bc6122344f996f3cb8f3d22
Paul Tobin <tobster111@gmail.com>
2020-09-16 14:45:26 +0100

    Using fs to read in a stream, __filename get the current file

---
+++ self-read/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const rs = fs.createReadStream(__filename);
+
+rs.on('data', (data) => {
+  console.log('Read chunk', data);
+});
+
+rs.on('end', () => {
+  console.log(`No more data`);
+});
